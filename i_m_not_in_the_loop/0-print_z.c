f76fc2a96e32ea617e0a13eace64f439199dc97c
dora <dora.korpar@gmail.com>
2016-02-05 03:56:36 +0000

    first two questions complete

---
+++ i_m_not_in_the_loop/0-print_z.c
@@ -0,0 +1,11 @@
+#include "my_functions.h"
+#include <unistd.h>
+
+void print_z(void)
+{
+  print_char('z');
+  print_char('\n');
+}
+
+
+
---
+++ i_m_not_in_the_loop/my_functions.h
@@ -0,0 +1,4 @@
+int print_char(char c);
+void print_z(void);
+void print_alphabet(void);
+
