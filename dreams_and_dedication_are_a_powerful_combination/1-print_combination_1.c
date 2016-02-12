f83e9eb647d2b9cd8e481235e3ca8c7c7e536e12
Dora Korpar <28@p2-5.hbtn.io>
2016-02-12 09:41:17 -0800

    finished number one

--- dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -2,6 +2,16 @@
 
 void print_combination_1(void)
 {
-
+  int n;
+  for (n = 0; n < 9; n++)
+    {
+      print_char((n + 48));
+      print_char(',');
+      print_char(' ');
+    }
+  if (n == 9)
+    {
+      print_char('9');
+    }
 }
 
