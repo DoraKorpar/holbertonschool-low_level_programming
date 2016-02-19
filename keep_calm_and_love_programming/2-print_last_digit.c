2edb8beb5a7e86b0fbb2fc3fcbd412a2b4419f1d
dora <dora.korpar@gmail.com>
2016-02-19 19:04:12 +0000

    finished all mandatory exercises

--- keep_calm_and_love_programming/2-print_last_digit.c
+++ keep_calm_and_love_programming/2-print_last_digit.c
@@ -1,6 +1,39 @@
 #include "my_functions.h"
 
-void print_last_digit(int)
-{
+void print_positive(int n);
 
-  int n;
+void print_last_digit(int n)
+{
+  if (n > 0) {
+    print_positive(n);
+  }
+  if (n == 0) {
+    print_char('0');
+  }
+  if (n < 0) {
+    n = -n;
+    print_positive(n);
+  }
+}
+
+void print_positive(int n)
+{
+  int print_me;
+  int power;
+  int digit;
+
+  digit = n;
+  power = 1;
+  while (digit > 9) {
+    digit /= 10;
+    power *= 10;
+  }
+
+  while (n > 0) {
+    print_me = n / power;
+    n = n % power;
+    power /= 10;
+    /*print_char(print_me + '0'); */
+  }
+  print_char(print_me + '0');
+}
