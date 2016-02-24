b3df78e0dc6e49b6b2c57acb31b7cecd9652fbaf
dora <dora.korpar@gmail.com>
2016-02-24 15:37:39 -0800

    did print number

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -1,12 +1,43 @@
 #include "my_functions.h"
+#include <limits.h>
 
-//not done. don't run!
+void print_number_pos(int n);
+
+/* this is the main function */
 void print_number(int n)
 {
-  char c;
-  c = n + '0';
-  for (print_char(c);)
+  if (n > 0)
     {
-      (n = c - '0');
+      print_number_pos(n);
+    }
+  if (n == 0)
+    {
+      print_char('0');
+    }
+  if (n < 0)
+    {
+      n = -n;
+      print_char('-');
+      print_number_pos(n);
+    }
+}
+
+void print_number_pos(int n)
+{
+  int n2 = n;
+  int power = 1;
+  int print;
+  while (n2 > 9)
+    {
+      n2 /= 10;
+      power *= 10;
+    }
+
+  while (n > 0)
+    {
+      print = n / power;
+      n = n % power;
+      power /= 10;
+      print_char(print + 48);
     }
 }
