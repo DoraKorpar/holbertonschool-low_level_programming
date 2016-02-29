173377026acf90af19a95305a29815aeef8171a6
dora <dora.korpar@gmail.com>
2016-02-29 14:53:30 -0800

    fixed compiling error

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -1,21 +1,23 @@
 #include <limits.h>
 
+int first_digit_pos(int n);
+
 int first_digit(int n)
 {
   if (n > 0) {
-    first_digit_pos(n);
+    return (first_digit_pos(n));
   }
-  if (n == 0) {
+  else if (n == 0) {
     return (0);
   }
-  if (n == INT_MIN) {
+  else if (n == INT_MIN) {
     n = n + 1;
     n = -n;
-    first_digit_pos(n);
+    return (first_digit_pos(n));
     }
-  if (n < 0) {
+  else {
     n = -n;
-    first_digit_pos(n);
+    return (first_digit_pos(n));
   }
 }
 
