ba83e32540b7c9693e2e7448391d3b19392b8f8f
dora <dora.korpar@gmail.com>
2016-03-11 18:52:56 -0800

    made third exercise account for negative numers

--- arrays_are_not_pointers_the_spoon_does_not_exist/2-main.c
+++ arrays_are_not_pointers_the_spoon_does_not_exist/2-main.c
@@ -1,11 +1,12 @@
+#include <limits.h>
 void print_array(int *a, int n);
 
 int main(void)
 {
   int array[5];
 
-  array[0] = 98;
-  array[1] = 402;
+  array[0] = -98;
+  array[1] = INT_MIN;
   array[2] = 198;
   array[3] = 298;
   array[4] = 1024;
--- arrays_are_not_pointers_the_spoon_does_not_exist/2-print_array.c
+++ arrays_are_not_pointers_the_spoon_does_not_exist/2-print_array.c
@@ -1,15 +1,16 @@
 void print_char(char c);
 void print_positive(int n);
+void print_number(int n);
 
 void print_array(int *a, int n)
 {
   int x = 0;
   while (x <= n-1) {
     if (x == n-1) {
-      print_positive(a[x]);
+      print_number(a[x]);
       print_char('\n');
     } else {
-      print_positive(a[x]);
+      print_number(a[x]);
       print_char(',');
       print_char(' ');
     }
@@ -17,12 +18,24 @@ void print_array(int *a, int n)
 }
 }
 
+void print_number(int n)
+{
+  if (n > 0) {
+    print_positive(n);
+  }
+  else if (n == 0) {
+    print_char('0');
+  }
+  else {
+    print_char('-');
+    n = -n;
+    print_positive(n);
+  }
+}
+
 void print_positive(int n)
 {
-  int print_me;
-  int power;
-  int digit;
-
+  int print_me; int power; int digit;
   digit = n;
   power = 1;
   while (digit > 9) {
