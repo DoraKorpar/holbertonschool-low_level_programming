9dc9f8d9f5c067656f344dde7c8c59573ec2833f
dora <dora.korpar@gmail.com>
2016-03-11 19:51:47 -0800

    completed mandatory

---
+++ make_magic_happen/0-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  *(p + 5) = 98;
+
+  printf("&(*(p + 5)) = %p\n", &(*(p + 5)));
+  printf("&p = %p\n", &p);
+  printf("&a[2] = %p\n", &a[2]);
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
