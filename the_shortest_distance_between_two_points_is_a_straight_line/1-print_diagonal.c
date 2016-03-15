35811bf2dc0bfd2353d36d903fd33f1c0d5e6598
dora <dora.korpar@gmail.com>
2016-03-15 13:30:54 -0700

    finished second exercise

--- the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
+++ the_shortest_distance_between_two_points_is_a_straight_line/1-print_diagonal.c
@@ -2,19 +2,27 @@ int print_char(char c);
 
 void print_diagonal(int n)
 {
-  int spaces = 0, n2 = n;
-  if (n > 0) {
-  while (n > 0) {
-    if (spaces < n2) {
-      print_char(' ');
-      spaces++;
-    }
+  int numSpace = 0, space2;
+  if (n <=  0) {
+    print_char('\n');
+  } else {
+    while (n > 0) {
+      space2 = numSpace;
+      while (space2 > 0) {
+	print_char(' ');
+	space2--;
+      }
     print_char('\\');
     print_char('\n');
     n--;
+    numSpace++;
   }
 }
-  else {
-    print_char('\n');
-}
+
+
+
+
+
+
+
 }
