408bec7656333cac7aefe890a300f4c7038da7d9
dora <dora.korpar@gmail.com>
2016-03-14 19:13:02 -0700

    finished first task

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/0-contact_strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+char *concat_strings(char *dest, const char *src)
+{
+  int i,  j;
+  for (i = 0; dest[i] != '\0'; i++);
+  for (j = 0; src[j] != '\0'; j++, i++) {
+    dest[i] = src[j];
+  }
+
+  dest[i] = '\0';
+  return (dest);
+}
