eaa97281da52482f034fecf9439ac8ff0af040bb
dora <dora.korpar@gmail.com>
2016-03-14 19:23:25 -0700

    finished second task and removed standard library from first

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/1-nconcat_strings.c
@@ -0,0 +1,10 @@
+char *nconcat_strings(char *dest, const char * src, int n)
+{
+  int i, j;
+  for (i = 0; dest[i] != '\0'; i++);
+  for (j = 0 ; j < n && src[j] != '\0' ; j++, i++) {
+    dest[i] = src[j];
+  }
+  dest[i] = '\0';
+  return dest;
+}
