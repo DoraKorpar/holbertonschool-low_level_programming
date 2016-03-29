144fd09be75fd543ffcc6bc8760ef19746e9e1e8
dora <dora.korpar@gmail.com>
2016-03-29 14:38:11 -0700

    finished fourth mandatory task

--- let_s_go_deeper/3-is_prime_number.c
+++ let_s_go_deeper/3-is_prime_number.c
@@ -1,3 +1,18 @@
+int prime_loop(int n, int i);
+
 int is_prime_number(int n) {
-  
+  if (n <= 1) {
+    return(0);
+  }
+  return(prime_loop(n, 2));
+}
+
+int prime_loop(int n, int i) {
+  if (n % i == 0) {
+    return(0);
+  }
+  if (i > 7) {
+    return(1);
+  }
+  return(prime_loop(n, ++i));
 }
