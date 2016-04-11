a3cf807848ce27b825ac146ba9ee89750cdf316c
dora <dora.korpar@gmail.com>
2016-04-11 15:46:26 -0700

    finished print grid

--- float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
+++ float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
-
+/* the idea is to iterate through the arrays at each index of the outer array and assign it to 0, but there's no way to check this yet */
 int **alloc_grid(int width, int height) {
+  int **outer_grid;
+  int i = 0, j = 0;
+  outer_grid = malloc(sizeof(int *) * width);
 
+  while (i < width) {
+    outer_grid[i] = malloc(sizeof(int) * height);
+
+    while (j < height) {
+      outer_grid[i][j] = 0;
+      j++;
+    }
+
+    i++;
+  }
+  return (outer_grid);
 }
+ 
+  
--- float_like_a_butterfly_sting_like_a_bee/3-main.c
+++ float_like_a_butterfly_sting_like_a_bee/3-main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int **alloc_grid(int, int);
-void print_grid(int **, int, int) {}
+void print_grid(int **, int, int);
 
 int main() {
   int **grid;
