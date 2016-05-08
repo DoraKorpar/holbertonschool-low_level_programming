ebc549b4a9aeb292daa959c184c2f375abcb9aef
dora <dora.korpar@gmail.com>
2016-05-08 11:49:59 -0700

    finished third task

--- linked_lists/1-params_to_list.c
+++ linked_lists/1-params_to_list.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include "list.h"
 char *string_dup(char *);
+int add_node(List **list, char *content);
 
 List *params_to_list(int ac, char **av) {
   List *list; 
+  int i;
+  list = NULL;
+  i = 0;
+    while (i < ac) {
+      add_node(&list, av[i]);
+      i++;
+    }
+  return (list);
 }
 
 char *string_dup(char *str) {
@@ -22,3 +31,30 @@ char *string_dup(char *str) {
   }
   return (str);
 }
+
+int add_node(List **list, char *content) {
+  List *newNode;
+  List *temp;
+  /* allocate mem to node */
+  newNode = malloc(sizeof(List));
+  if (newNode == NULL) {
+    return (1);
+  }
+  /* populate node with copy of param string */
+  newNode->str = string_dup(content);
+  /* next points to NULL because the new node will be last in the list */
+  newNode->next = NULL;
+  /* if statement assigns newNode location to *list if it is the first node added to the list */
+  if (*list == NULL) {
+    *list = newNode;
+  }
+  else {
+    temp = *list;
+    while (temp->next != NULL) {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+  }
+  return (0);  
+}
+ 
