9a7938ce320dc8ad3cbc3e54dafd9d9c8b25c948
dora <dora.korpar@gmail.com>
2016-05-10 22:47:06 -0700

    removed print_char

--- file_descriptors/0-print_content/main.c
+++ file_descriptors/0-print_content/main.c
@@ -5,11 +5,9 @@
 
 #define BUFF_SIZE 4096
 
-int print_char(char c);
-
 int main(int ac, char **av) {
-  int fd, len, i;
-  char buffer[BUFF_SIZE + 1];
+  int fd, len;
+  char buffer[BUFF_SIZE];
   /* handles error if there aren't enough arguments */
   if (ac != 2) {
     return (1);
@@ -18,17 +16,16 @@ int main(int ac, char **av) {
   if (fd == -1) {
     return (1);
   }
+  
   len = read(fd, buffer, BUFF_SIZE);
   if (len == -1) {
     close(fd);
     return (1);
   }
 
-  buffer[len] = '\0';
-  i = 0;
-  while (buffer[i] != '\0') {
-    print_char(buffer[i]);
-    i++;
+  if ((write(1, buffer, len)) == -1) {
+    close(fd);
+    return (1);
   }
   close(fd);
   return (0);
