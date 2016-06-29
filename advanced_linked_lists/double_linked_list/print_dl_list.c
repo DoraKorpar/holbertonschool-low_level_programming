34984a8515df99b8189dc64a01a3efe860dabd18
dora <dora.korpar@gmail.com>
2016-06-29 10:54:40 -0700

    finished print

--- advanced_linked_lists/double_linked_list/print_dl_list.c
+++ advanced_linked_lists/double_linked_list/print_dl_list.c
@@ -3,6 +3,7 @@
 
 void print_string(char *);
 int print_char(char);
+void print_last_node(List *);
 
 void print_dl_list(List *list)
 {
@@ -22,15 +23,20 @@ void print_dl_list(List *list)
       print_string(node->prev->str);
       print_char('\n');
     }
-
-    if (node->next == NULL) {
-      print_string("\tNULL\n");
-    }
-    else {
-      print_char('\t');
-      print_string(node->next->str);
-      print_char('\n');
-    }
+    print_char('\t');
+    print_string(node->next->str);
+    print_char('\n');
     node = node->next;
   }
+  print_last_node(node);
+}
+
+void print_last_node(List *node)
+{
+  print_string(node->str);
+  print_char('\n');
+  print_char('\t');
+  print_string(node->prev->str);
+  print_char('\n');
+  print_string("\tNULL\n");
 }
