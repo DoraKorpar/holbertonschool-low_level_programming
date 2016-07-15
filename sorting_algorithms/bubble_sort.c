a6dfd2b109986dd4623aabfd5f0494bec0b36083
dora <dora.korpar@gmail.com>
2016-07-14 17:25:27 -0700

    added comments

--- sorting_algorithms/bubble_sort.c
+++ sorting_algorithms/bubble_sort.c
@@ -3,17 +3,19 @@ void bubble_sort(int *array, int size)
   int i, tmp, loop;
 
   i = 0;
+  /* loop is one less than size b/c array starts at 0 */
   loop = size - 1;
-
   while (loop > 0) {
     while ((i + 1) < size) {
       if (array[i] > array[i + 1]) {
+	/* switches number at array indeces if conditions are met */
 	tmp = array[i];
 	array[i] = array[i + 1];
 	array[i + 1] = tmp;
       }
       i++;
     }
+    /* i reinitialized to 0 to restart at beginning of array for every loop */
     i = 0;
     loop--;
   }
--- sorting_algorithms/insertion_sort.c
+++ sorting_algorithms/insertion_sort.c
@@ -3,10 +3,11 @@ void insertion_sort(int * array, int size)
   int i, j, tmp;
 
   i = 1;
-
   while (i < size) {
+    /* j initialized to i because with every loop, inner loop begins at next increment */
     j = i;
     while (j > 0 && array[j] < array[j-1]) {
+      /* switches numbers in array if conditions met */
       tmp = array[j];
       array[j] = array[j-1];
       array[j-1] = tmp;
