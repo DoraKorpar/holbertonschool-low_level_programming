402a182df825f15af6a7743def1685b01be5a1df
dora <dora.korpar@gmail.com>
2016-07-23 13:54:15 -0700

    fixed comments for column width requirement

--- trees/btree_insert.c
+++ trees/btree_insert.c
@@ -25,17 +25,17 @@ int btree_insert(BTree **tree, char *data)
 	node = malloc(sizeof(BTree)); /* allocate space in memory for new node */
 	if (node == NULL)
 		return (1);
-	node->str = strdup(data); /* puts copy of data string into the str member of struct node */
+	node->str = strdup(data); /* puts copy of data string into the str */
 	if (node->str == NULL)
 		return (1);
-	node->left = NULL; /* the new node always inserted at end of tree, so points to NULL */
+	node->left = NULL; /* new node inserted at end of tree, so points to NULL */
 	node->right = NULL;
 	if (*tree == NULL) /* means there are no previous nodes */
 	  *tree = node;
 	else
 		{
 			temp = *tree;
-			ret = traverse_tree(temp, node); /* function will insert node at proper place */
+			ret = traverse_tree(temp, node); /* insert node at proper place */
 			if (ret == 1)
 				return (1);
 		}
@@ -43,27 +43,29 @@ int btree_insert(BTree **tree, char *data)
 }
 
 /**
- * traverse_tree - moves through tree recursively to place new node in correct place
+ * traverse_tree - moves thru tree recursively to put new node in correct place
  * @temp: pointer to first node of tree
  * @node: pointer to node to be inserted
  *
- * Compares str elements of both nodes and goes right or left based on outcome.
- * Recursively calls function to traverse through tree until next node pointed to
- * is NULL, where new node will be inserted.
+ * Compares str elements of both nodes with strcmp and goes right or left based
+ * on outcome. strcmp returns 0 if strings are equal, <0 if 2nd string is
+ * greater, and >0 if second string is smaller.
+ * Recursively calls function to traverse through tree until next node pointed
+ * to is NULL, where new node will be inserted.
  */
 int traverse_tree(BTree *temp, BTree *node)
 {
 	int comp;
 
-	comp = strcmp(temp->str, node->str); /* function outputs 0 if strings are equal, <0 if string2 is greater, and >0 if string2 is smaller */
+	comp = strcmp(temp->str, node->str); /* determine where to put new node */
 	if (comp < 0 || comp == 0)
 		{
-			if (temp->right != NULL) /* continues looping through tree is next pointer is not NULL */
+			if (temp->right != NULL) /* loop thru tree til nxt ptr isn't NULL */
 				{
 					temp = temp->right;
 					traverse_tree(temp, node);
 				}
-			else /* if next pointer is NULL, that means end of branch, and new node is inserted */
+			else /* if nxt ptr NULL, means end of branch, & new node inserted */
 				{
 					temp->right = node;
 					node->right = NULL;
