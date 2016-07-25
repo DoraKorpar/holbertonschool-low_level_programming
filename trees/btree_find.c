a0c63f5c117dffda1c507ba57a6e3da102d2f1f9
dora <dora.korpar@gmail.com>
2016-07-25 15:29:18 -0700

    more betty stuff

--- trees/btree_find.c
+++ trees/btree_find.c
@@ -21,9 +21,9 @@ BTree *btree_find(BTree *tree, __attribute__((unused))char *str)
 
 	temp = tree;
 	if (tree == NULL || str[0] == '\0') /* ensures both params not NULL */
-		return NULL;
+		return (NULL);
 	found = find_node(temp, str); /* calls recursive function */
-	return found;
+	return (found);
 }
 
 /**
@@ -47,26 +47,26 @@ BTree *find_node(BTree *temp, char *str)
 
 	comp = strcmp(temp->str, str); /* compares str of node to param */
 	if (comp == 0)
-		return temp;
+		return (temp);
 	else if (comp < 0) /* if return is <0, string2 greater than string1 */
+	{
+		if (temp->right != NULL) /* moves ptr to nxt node */
 		{
-			if (temp->right != NULL) /* moves ptr to nxt node */
-				{
-					temp = temp->right;
-					found = find_node(temp, str);
-				}
-			else /* if next node is NULL, then there is no match */
-				return NULL;
+			temp = temp->right;
+			found = find_node(temp, str);
 		}
+		else /* if next node is NULL, then there is no match */
+			return (NULL);
+	}
 	else if (comp > 0) /* string2 < string1, so to left of tree */
+	{
+		if (temp->left != NULL)
 		{
-			if (temp->left != NULL)
-				{
-					temp = temp->left;
-					found = find_node(temp, str);
-				}
-			else /* if nxt node NULL, then there is no match */
-				return NULL;
+			temp = temp->left;
+			found = find_node(temp, str);
 		}
-	return found;
+		else /* if nxt node NULL, then there is no match */
+			return (NULL);
+	}
+	return (found);
 }
