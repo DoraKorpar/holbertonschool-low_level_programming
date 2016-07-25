3cff4ca8535748fe33d948ac7831332b70b782da
dora <dora.korpar@gmail.com>
2016-07-25 15:01:22 -0700

    copmliant with betty

--- trees/array_to_btree.c
+++ trees/array_to_btree.c
@@ -6,7 +6,7 @@
  * array_to_btree - function takes array and creates a binary tree
  * @array: array is an array of strings, each of which will populate a
  * node of the tree
- * 
+ *
  * Each string in the array will become the str element of the BTree
  * struct of the new node being created. The function btree_insert is
  * described in the eponymous file.
@@ -19,13 +19,13 @@ BTree *array_to_btree(char **array)
 	int ret;
 
 	tree = NULL; /* initializing pointer to tree */
-	i = 0; 
+	i = 0;
 	while (array[i]) /* only works if final element of array is NULL */
 		{
 			ret = btree_insert(&tree, array[i]);
 			if (ret == 1)
-				return (NULL);
+				return NULL;
 			i++;
 		}
-	return (tree);
+	return tree;
 }
