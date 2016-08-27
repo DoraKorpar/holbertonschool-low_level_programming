57a36fe9885b3ee154e981a11d4d1f4728b3a48c
dora <dora.korpar@gmail.com>
2016-08-27 10:15:56 -0700

    vscode commit

--- maze/header.h
+++ maze/header.h
@@ -10,17 +10,13 @@ typedef struct SDL_Instance
 	SDL_Renderer *renderer;
 } SDL_Instance;
 
-int init_instance (SDL_Instance *);
-
 #define ISO_Y 1
 #define ISO_X (sqrt(3.0))
 #define ISO_HYP 2
 #define WIN_WIDTH 1260
 #define WIN_HEIGHT 720
 
-void draw_grid_alt(SDL_Instance/*, int, int*/);
-void draw_lines2(SDL_Instance, int, int);
-void add_alt(SDL_Instance, int, int, int altitudes[][8]);
+int init_instance (SDL_Instance *);
 int poll_events(void);
 
 #endif
