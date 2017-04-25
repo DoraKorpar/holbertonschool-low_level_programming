fe1b56bf9f211aabc39d924e7415161d760ba8f6
dora <dora.korpar@gmail.com>
2017-04-24 21:54:33 -0700

    Fix render var declaration

--- maze/src/render_bg.c
+++ maze/src/render_bg.c
@@ -2,10 +2,15 @@
 
 void render_bg(t_instance *instance)
 {
+	SDL_Rect floor;
+
 	SDL_SetRenderDrawColor(instance->renderer, 0, 191, 255, 255);
 	/* blue sky */
 	SDL_RenderClear(instance->renderer);
-	SDL_Rect floor = {0, WIN_HEIGHT / 2, WIN_WIDTH, WIN_HEIGHT / 2};
+	floor.x = 0;
+	floor.y = WIN_HEIGHT;
+	floor.w = WIN_WIDTH;
+	floor.h = WIN_HEIGHT / 2;
 
 	SDL_SetRenderDrawColor(instance->renderer, 50, 205, 50, 255);
 	/* green grass */
