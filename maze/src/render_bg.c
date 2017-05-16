f1631fda9726cd447b955c7c04579a680be5f215
dora <dora.korpar@gmail.com>
2017-05-16 07:06:12 -0700

    Fix floor

--- maze/src/render_bg.c
+++ maze/src/render_bg.c
@@ -2,16 +2,21 @@
 
 void render_bg(t_instance *instance)
 {
+	SDL_Rect sky;
 	SDL_Rect floor;
 
+	sky.x = 0;
+	sky.y = 0;
+	sky.w = WIN_WIDTH;
+	sky.h = WIN_HEIGHT / 2;
 	SDL_SetRenderDrawColor(instance->renderer, 0, 191, 255, 255);
 	/* blue sky */
-	SDL_RenderClear(instance->renderer);
+	SDL_RenderFillRect(instance->renderer, &sky);
+	
 	floor.x = 0;
 	floor.y = WIN_HEIGHT / 2;
 	floor.w = WIN_WIDTH;
 	floor.h = WIN_HEIGHT / 2;
-
 	SDL_SetRenderDrawColor(instance->renderer, 50, 205, 50, 255);
 	/* green grass */
 	SDL_RenderFillRect(instance->renderer, &floor);
