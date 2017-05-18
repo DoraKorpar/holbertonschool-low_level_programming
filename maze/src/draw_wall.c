b04b4539f8010a47b8bbbb712bd0ef416dd22720
dora <dora.korpar@gmail.com>
2017-05-18 07:12:37 -0700

    Add comments

--- maze/src/draw_wall.c
+++ maze/src/draw_wall.c
@@ -20,8 +20,10 @@ void draw_wall(float wall_dist, int col_num, int v_or_h,
 	y_start = (WIN_HEIGHT / 2) - (wall_height / 2);
 	y2 = y_start + wall_height;
 
+	/* wall will run north-south */
 	if (v_or_h == 0)
 		SDL_SetRenderDrawColor(instance->renderer, 255, 0, 0, 128);
+	/* wall will run east-west */
 	if (v_or_h == 1)
 		SDL_SetRenderDrawColor(instance->renderer, 250, 250, 0, 128);
 	SDL_RenderDrawLine(instance->renderer,
